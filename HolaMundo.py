7f269581c493fcd156f1099cf27bed4a2421808d
DGB014 <152391567+DGB014@users.noreply.github.com>
2023-11-29 12:13:08 +0100

    Create HolaMundo.py

---
+++ HolaMundo.py
@@ -0,0 +1,17 @@
+print("Empezamos")
+
+a = 5
+print(a)
+#Comentario
+b = input("Introduce algo:")
+
+cont = 10
+if(cont==10):
+    print("Es igual")
+
+cont = 1
+while(cont<=10):
+    print(cont)
+    cont +=1
+
+print("Fin")
